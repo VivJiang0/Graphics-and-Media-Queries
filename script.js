71704cc3595a7d62a28f7d94dcfe862a6d18f653
Vivienjiang03 <94229288+VivJiang0@users.noreply.github.com>
2022-01-22 15:13:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+     var canvas = document.getElementById("my-first-canvas"); // variable that attaches to your html document
+   var context = canvas.getContext("2d"); // getContext decides the “type” of canvas
+   // x and y coordinates always start at the top left of an element or the page
+   context.moveTo(0, 0);  // x, y coordinates where the line starts
+   context.lineTo(200,100); //x, y coordinates where the line ends
+   context.stroke(); // function that creates the line, won’t work without this codec
+</script>
